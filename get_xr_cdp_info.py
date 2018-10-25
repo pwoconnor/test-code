2c45f335cc59e47cfa42094f84cd4862732c4a3b
fallenarc <41013523+fallenarc@users.noreply.github.com>
2018-10-24 22:45:39 -0500

    String formatting and docstrings

--- get_xr_cdp_info.py
+++ get_xr_cdp_info.py
@@ -1,6 +1,6 @@
 import os
 from os import listdir
-from os.path import isfile,join
+from os.path import isfile,join,expanduser
 import json
 import time
 import shutil
@@ -20,7 +20,7 @@ import pandas as pd
 
 
 #set working directory for tmp files
-wd = ('/home/{user dir}/python/temp/')
+wd = expanduser('~/python/temp/')
 #set up threading to utilize (max_threads count) concurrent SSH connections
 threads = []
 max_threads = 100
@@ -35,9 +35,8 @@ mpass = getpass.getpass("Enter MySQL Password: ")
 
 #-----------------------------------------------------------
 def del_temp_files():
-    '''
-    Delete temporary files that were created on the previous run
-    '''
+    """Delete temporary files that were created on the previous run"""
+
     # temp_files_dir = (wd)
     list_temp_dir = os.listdir(wd)
     ext = (".json",".csv",".txt",".log")
@@ -47,9 +46,8 @@ def del_temp_files():
 
 #------------------------------------------------------------
 def get_db():
-    '''
-    MySQL connection
-    '''
+    """MySQL connection"""
+
     # assgin var for MySQL database connection
     db = MySQLdb.connect(host="localhost",
                          user=muser,
@@ -60,11 +58,10 @@ def get_db():
 
 #-----------------------------------------------------------
 def devices_list():
-    '''
-    Pull list of IPs from MySQL to pass onto netmiko connections
-    '''
+    """Pull list of IPs from MySQL to pass onto netmiko connections"""
+
     cursor = db.cursor()
-    cursor.execute("SELECT DISTINCT IP FROM *TABLE WHERE *FEILD LIKE 'string' AND  *FEILD LIKE '%string%' limit 10")
+    cursor.execute("SELECT DISTINCT IP FROM RC_MDL WHERE netype LIKE 'MKT Router' AND  model_name LIKE '%ASR%' limit 10")
     items = cursor.fetchall()
     devices = [x[0] for x in items] #convert tuple to list
     #print (devices)
@@ -72,8 +69,8 @@ def devices_list():
     return devices
 
 #------------------------------------------------------------
-#open SSH connection to device
 def ssh_connection(ip, username, password):
+    """open SSH connection to device"""
     try:
         return ConnectHandler(device_type='cisco_xr',
                                 ip=ip,
@@ -83,54 +80,49 @@ def ssh_connection(ip, username, password):
 
     except Exception as error:
         logger.error('. %&%&%&%&%&  {}   \t   {}'.format(ip, error))
-        with open (wd +"conn_error.txt", "a") as efile:
+        with open ("{}conn_error.txt".format(wd), "a") as efile:
             efile.write(ip+"\n")
 
 #--------------------------------------------------------------
 def get_worker(ip,device):
-    '''
-     Gather info using NTC Templates for output
-    '''
+    """Gather info using NTC Templates for output"""
+
     try:
 
-        print("Connecting to "+ip)
+        print("Connecting to {}".format(ip))
         set_term = device.send_command("terminal length 0")
         cdp_info = device.send_command('show cdp neighbor detail', use_textfsm=True)
 
-        #inserting additional information loop
+
         for x in cdp_info:
-            #insert IP of device into dict for identification purposes
             x["ip"] = ip
             # print(x["dest_host"].split(".")[0])
-            # these hostnames sometimes have .domain-name.net. I split that out as I do not need/want the domain name
             x["dest_host"] = x["dest_host"].split(".")[0]
             x["sysname"] = x["sysname"].split(".")[0]
 
 
 
-        with open (wd +"temp_cdp_info_"+(ip)+".json", "w") as file1:
+        with open ("{}temp_cdp_info_{}.json".format(wd,ip), "w") as file1:
             json.dump(cdp_info,file1)
 
     except Exception as error:
-        print("Get_ERROR - " +str(error) + " " + str(ip))
+        print("Get_ERROR - {}, {}".format(ip,error))
         logger.error(". Get Error    {}   \t    {}".format(ip, error))
         #change file path to your directory
         with open (wd +"conn_error.txt", "a") as efile:
             if 'unicode' in str(error):
-                with open (wd +"conn_error_unicode.txt", "a") as efile2:
+                with open ("{}conn_error_unicode.txt".format(wd), "a") as efile2:
                     efile2.write(ip+"\n")
                     devices.remove(ip)
             else:
                 efile.write(ip+"\n")
 #--------------------------------------------------------------
 def retry_errors():
-    '''
-    Retrying devices that were logged in the expection section of get_worker
-    '''
+    """Retrying devices that were logged in the expection section of get_worker"""
 
-    if os.path.isfile(wd +'conn_error.txt'):
+    if os.path.isfile('{}conn_error.txt'.format(wd)):
         print("Retrying errors")
-        hosts = open(wd +"conn_error.txt", "r").readlines()
+        hosts = open("{}conn_error.txt".format(wd), "r").readlines()
 
         for host in hosts:
             try:
@@ -143,43 +135,40 @@ def retry_errors():
                                         global_delay_factor=2,
                                         verbose=False
                                         )
-                print("Connecting to "+host)
+                print("Connecting to {}".format(host))
 
                 set_term = device.send_command("terminal length 0")
                 cdp_info = device.send_command('show cdp neighbor detail', use_textfsm=True)
 
-                #inserting additional information loop
+
                 for x in cdp_info:
-                    #insert IP of device into dict for identification purposes
                     x["ip"] = host
                     # print(x["dest_host"].split(".")[0])
-                    # these hostnames sometimes have .domain-name.net. I split that out as I do not need/want the domain name
                     x["dest_host"] = x["dest_host"].split(".")[0]
                     x["sysname"] = x["sysname"].split(".")[0]
 
 
-                with open (wd + "temp_cdp_info_"+(host)+".json", "w") as file1:
+                with open ("{}temp_cdp_info_{}.json".format(wd,host), "w") as file1:
                     json.dump(cdp_info,file1)
 
             except Exception as error:
-                print ("SECOND_ERROR - " +str(error) + "" + str(host))
+                print ("SECOND_ERROR - {}, {}".format(host,erro))
                 logger.error(". Get Error    {}   \t    {}".format(host, error))
 
 
 #-------------------------------------------------------------
 def wait_time():
         try:
-            while ("temp_cdp_info_" + devices[-1] + ".json") not in os.listdir(wd):
+            while ("temp_cdp_info_{}.json".format(devices[-1])) not in os.listdir(wd):
                 print("Waiting...")
                 time.sleep(5)
         except Exception as e100:
-            print("DEVICE LIST ERROR: " + str(e100))
+            print("DEVICE LIST ERROR: {}".foramt(e100))
 
 #-------------------------------------------------------------
 def conv_csv():
-    '''
-    Convert JSON to CSV
-    '''
+    """Convert JSON to CSV"""
+
     path = (wd)
     os.chdir(wd)
     print("Converting files to CSV")
@@ -187,13 +176,12 @@ def conv_csv():
         if file.endswith(".json"):
             out_filename = file.split(".json")[0]
             df=pd.read_json(file)
-            df.to_csv("results_"+ out_filename +".csv", header=False, index=False)
+            df.to_csv("results_{}.csv".format(out_filename), header=False, index=False)
 
 #-------------------------------------------------------------
 def insert_data():
-    '''
-    Insert data from CSV file into MySQL
-    '''
+    """Insert data from CSV file into MySQL"""
+
     print("Sending data to MySQL")
     os.chdir(wd)
     cursor = db.cursor()
@@ -218,7 +206,7 @@ def insert_data():
 #--------------------------------------------------------------
 
 def main(ip, username, password):
-    # connect to device via SSH
+    """Connect to device using netmiko"""
     device = ssh_connection(ip, username, password)
 
     #if we can't connect, release the lock and move on
@@ -238,10 +226,10 @@ if __name__ == '__main__':
     del_temp_files()
     #set up logging
     logger = logging.getLogger("LOG")
-    handler = logging.FileHandler(wd +"main.log")
+    handler = logging.FileHandler("{}main.log".format(wd))
     logger.setLevel(logging.DEBUG)
     logger.addHandler(handler)
-    paramiko.util.log_to_file(wd +"paramiko.log")
+    paramiko.util.log_to_file("{}paramiko.log".format(wd))
     #set up MySQL connection
     db = get_db()
     #grab list of IP addresses
